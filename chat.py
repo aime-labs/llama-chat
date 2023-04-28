184b70fe88f515f6d7f38c80ee9f33ce85324fb6
carlovogel <carlo@aime.info>
2023-04-28 14:09:27 +0000

    Changed print output to token by token without repeating full context

--- chat.py
+++ chat.py
@@ -5,9 +5,9 @@ from typing import Tuple
 import os
 import sys
 import torch
-import fire
 import time
 import json
+import argparse
 
 from pathlib import Path
 
@@ -36,6 +36,7 @@ def load(
     world_size: int,
     max_seq_len: int,
     max_batch_size: int,
+
 ) -> LLaMA:
     start_time = time.time()
     checkpoints = sorted(Path(ckpt_dir).glob(f'merged.{world_size}GPUs.*.pth'))
@@ -58,33 +59,29 @@ def load(
     model = Transformer(model_args)
     torch.set_default_tensor_type(torch.FloatTensor)
     model.load_state_dict(checkpoint, strict=False)
+    
 
     generator = LLaMA(model, tokenizer)
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
     return generator
 
 
-def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
-    temperature: float = 0.8,
-    top_p: float = 1,
-    max_seq_len: int = 2048,
-    max_batch_size: int = 8,
-):
+def main():
     local_rank, world_size = setup_model_parallel()
     if local_rank > 0:
         sys.stdout = open(os.devnull, "w")
+    args=load_flags()
 
     generator = load(
-        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+        args.ckpt_dir, args.tokenizer_path, local_rank, world_size, args.max_seq_len, args.max_batch_size
     )
 
     ctx = """A dialog, where User interacts with AI. AI is helpful, kind, obedient, honest and very reasonable.
 User: Hello, AI.
 AI: How can I assist you today?
 """        
-
+    sampler = 'top_k' if not args.use_top_p else 'top_p'
+    print(ctx)
     while True:
         prompts = []
 
@@ -97,12 +94,50 @@ AI: How can I assist you today?
         torch.distributed.broadcast_object_list(prompts, 0)
 
         results = generator.generate(
-            prompts, max_gen_len=512, temperature=temperature, top_p=top_p
+            prompts, max_gen_len=512, temperature=args.temperature, top_p=args.top_p, top_k=args.top_k, repetition_penalty=args.repetition_penalty, sampler=sampler
         )
-
         ctx = results[0]
-        print(ctx)
-    
+
+def load_flags():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ckpt_dir", type=str, required=False,
+        help="Location of LLama weights",
+    )
+    parser.add_argument(
+        "--tokenizer_path", type=str, required=False,
+        help="Location of tokenizer"
+    )
+    parser.add_argument(
+        '--temperature', type=float, default=0.8, required=False,
+    help='Temperature'
+                    )
+    parser.add_argument(
+        "--top_p", type=float, default=0.9, required=False,
+        help="Top_p, 0=<top_p<=1"
+    )
+    parser.add_argument(
+        "--top_k", type=int, default=40, required=False,
+        help="Top_k, 0=<top_k<=1",
+    )
+    parser.add_argument(
+        "--max_seq_len", type=int, default=2048, required=False,
+        help="Maximum sequence length",
+    )
+    parser.add_argument(
+        "--max_batch_size", type=int, default=1, required=False,
+        help="Maximum batch size",
+    )    
+    parser.add_argument(
+        "--repetition_penalty", type=float, default=(1.0/0.85), required=False,
+        help="Top_k, 0=<top_k<=1",
+    )
+    parser.add_argument(
+        '--use_top_p', action='store_true', required=False,
+        help='Use top_p sampler instead of top_k'
+    )
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    main()
--- llama/generation.py
+++ llama/generation.py
@@ -15,11 +15,14 @@ class LLaMA:
         self.tokenizer = tokenizer
 
     def generate(
-        self,
-        prompts: List[str],
-        max_gen_len: int,
-        temperature: float = 0.8,
-        top_p: float = 0.95,
+            self,
+            prompts: List[str],
+            max_gen_len: int,
+            temperature: float = 0.8,
+            top_p: float = 0.95,
+            top_k: int = 40,
+            repetition_penalty: float = (1.0 / 0.85),
+            sampler: str = 'top_k',
     ) -> List[str]:
         bsz = len(prompts)
         params = self.model.params
@@ -37,41 +40,81 @@ class LLaMA:
         tokens = torch.full((bsz, total_len), self.tokenizer.pad_id).cuda().long()
         for k, t in enumerate(prompt_tokens):
             tokens[k, : len(t)] = torch.tensor(t).long()
+            tokens[k, -1] = self.tokenizer.eos_id
         input_text_mask = tokens != self.tokenizer.pad_id
         start_pos = min_prompt_size
         prev_pos = 0
         decoded = [None] * bsz
+        word = []
 
         for cur_pos in range(start_pos, total_len):
             logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            # repetition penalty from CTRL paper (https://arxiv.org/abs/1909.05858)
+            if repetition_penalty != 1.0:
+                logits_new = logits.clone()
+                batch_size = len(tokens)
+                for i in range(batch_size):
+                    for token in set(tokens[i].tolist()):
+                        # if score < 0 then repetition penalty has to multiplied to reduce the previous token probability
+                        if logits[i, token] < 0:
+                            logits_new[i, token] = logits[i, token] * repetition_penalty
+                        else:
+                            logits_new[i, token] = logits[i, token] / repetition_penalty
+                logits = logits_new
+
             if temperature > 0:
                 probs = torch.softmax(logits / temperature, dim=-1)
-                next_token = sample_top_p(probs, top_p)
+                if sampler == 'top_k':
+                    next_token = sample_top_k(probs, top_p=top_p, top_k=top_k)
+                else:
+                    next_token = sample_top_p(probs, top_p)
             else:
                 next_token = torch.argmax(logits, dim=-1)
+                           
             next_token = next_token.reshape(-1)
             # only replace token if prompt has already been generated
             next_token = torch.where(
                 input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
             )
             tokens[:, cur_pos] = next_token
+            if self.tokenizer.decode(next_token.item()) != 'User':
+                word.append(next_token.item())
+            word_str = self.tokenizer.decode(word)
+
+            if " " in word_str:
+                print(word_str[:word_str.find(" ")+1], end='')
+                word = word[-1:]
+
             prev_pos = cur_pos
 
             for i, t in enumerate(tokens.tolist()):
+                # i = cur_pos
+                # t = next_token
                 # cut to max gen len
+                # t = t[: len(prompt_tokens[i]) + max_gen_len]
                 t = t[: min(cur_pos, len(prompt_tokens[i]) + max_gen_len)]
+                
                 # cut to eos tok if any
                 try:
                     t = t[: t.index(self.tokenizer.eos_id)]
                 except ValueError:
                     pass
-                d = self.tokenizer.decode(t)    
-                decoded[i] = d
 
-                result_count_newlines = d.count("\n")
-                if result_count_newlines > count_newlines:
-                    return decoded
+                try:
+                    d = self.tokenizer.decode(t)
+                    decoded[i] = d                    
+                    result_count_newlines = d.count("\n")
+                    if result_count_newlines > count_newlines:
+                        word=word[:-1]
+                        word_str = self.tokenizer.decode(word)
+                        print(word_str)
+                        return decoded
 
+                except IndexError:
+                    traceback.print_exc()
+                    print(t)
+
+        
         return decoded
 
 
@@ -81,6 +124,22 @@ def sample_top_p(probs, p):
     mask = probs_sum - probs_sort > p
     probs_sort[mask] = 0.0
     probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+    next_token = torch.multinomial(probs_sort, num_samples=1)
+    next_token = torch.gather(probs_idx, -1, next_token)
+    return next_token
+
+# sampler by Shawn
+def sample_top_k(probs, top_p=0.0, top_k=40):
+    if top_k > 0:
+        probs_sort, probs_idx = torch.topk(probs, top_k)
+    else:
+        probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+    if top_p > 0.0:
+        probs_sum = torch.cumsum(probs_sort, dim=-1)
+        mask = probs_sum - probs_sort > top_p
+        probs_sort[mask] = 0.0
+        probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+
     next_token = torch.multinomial(probs_sort, num_samples=1)
     next_token = torch.gather(probs_idx, -1, next_token)
     return next_token
